5fcc16dbe7f25c8cc03b9f50de3923bdd5b708c8
Davide Murro <118051417+davide-murro@users.noreply.github.com>
2025-11-27 22:06:38 +0100

    test  build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/slot-game/" : "/", // base URL for GitHub Pages
+
+  build: {
+    outDir: "docs", // output folder for the build
+    emptyOutDir: true, // clean folder before build
+  },
   server: {
     port: 8080,
     open: true,
   },
-});
+}));
